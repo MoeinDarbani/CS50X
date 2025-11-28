dcd72e6a80f84e4a72bd170f588d8733a6fa18a7
Moein Darbani <77524183+MoeinDarbani@users.noreply.github.com>
2025-11-28 20:20:22 +0330

    Create Less Comfortable.c

---
+++ pset1/Mario/Less Comfortable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    float num;
+    do
+    {
+        printf("How many bricks do you want: ");
+        scanf("%f", &num);
+    }
+    while (num <= 0 || num != (int) num);
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
